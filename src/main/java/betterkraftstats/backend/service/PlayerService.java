effc9dd9c59909d6e00e0495a1600849730dc958
RadVonSwag <drewstes1@gmail.com>
2022-08-08 21:55:26 -0500

    figure out hashmap merge

--- src/main/java/betterkraftstats/backend/service/PlayerService.java
+++ src/main/java/betterkraftstats/backend/service/PlayerService.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -39,21 +38,38 @@ public class PlayerService {
 
     public Map<String, Integer> getTimePlayed() {
         Map<String, Integer> uuidAndTime = new HashMap<String, Integer>();
+        Map<String, String> usernameAndTime = new HashMap<String, String>();
 
         File[] statsFiles = new File(stats_dir).listFiles();
         for (int i = 0; i < statsFiles.length; i++) {
             File currentFile = statsFiles[i];
             try {
                 JsonNode currentPlayer = mapper.readTree(currentFile);
-                String timePlayedAsString = currentPlayer.get("stats").get("minecraft:custom").get("minecraft:play_one_minute")
+                String timePlayedAsString = currentPlayer.get("stats").get("minecraft:custom")
+                        .get("minecraft:play_one_minute")
                         .toString();
                 int timePlayed = Integer.parseInt(timePlayedAsString) / 72000;
                 String uuid = currentFile.getName().substring(0, 35);
                 uuidAndTime.put(uuid, timePlayed);
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+
+        JsonNode playerList;
+        Map<String, String> uuidAndUsername = new HashMap<String, String>();
+        try {
+            playerList = mapper.readTree(new File(usercache_dir));
+            for (int i = 0; i < playerList.size(); i++) {
+                uuidAndUsername.put(playerList.get(i).get("uuid").toString(), playerList.get(i).get("name").toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //uuidAndUsername.merge(key, value, mergeBiFunction); -- Figure this shit out????
         return uuidAndTime;
     }
+
 }
